638f52ef6b9603758b41f7754ea3254f48d656a2
nabil-el-allaoui <hassi258456@gmail.com>
2025-03-25 14:12:54 +0000

    Implement CRUD operations for whitelist management

--- routes/api.php
+++ routes/api.php
@@ -2,10 +2,13 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
-
-
+use App\Http\Controllers\WhitelistController;
 
 Route::post('/register',[AuthController::class,'register']);
 Route::post('/login',[AuthController::class,'login']);
 Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:api');
 Route::post('/refresh', [AuthController::class, 'refresh'])->middleware('auth:api');
+
+Route::middleware('auth')->group(function(){
+    Route::apiResource('/whitelist', WhitelistController::class);
+});
