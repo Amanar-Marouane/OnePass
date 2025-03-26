6617c955d1eccebff11cff0de2c513c3686da51f
ismail <igaxowni@gmail.com>
2025-03-26 11:07:26 +0000

    set the functionality to send email to user if he loged with diffirent mac address

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Middleware\RateLimiter;
 use Illuminate\Support\Facades\Route;
 
 
-Route::group(['middleware' => RateLimiter::class], function () {
-    Route::get('/', [App\Http\Controllers\test::class, 'index']);
+
+Route::get('/', function () {
+    return view('welcome');
 });
