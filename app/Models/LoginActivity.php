98bc03bafe232b239dc6c602be71ee75c61fa911
mohamed <mohamedboukab202@gmail.com>
2025-03-26 12:50:22 +0000

    create loginActivity table

---
+++ app/Models/LoginActivity.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class LoginActivity extends Model
+{
+     use HasFactory;
+
+
+     protected $table = "login_activity";
+     protected $fillable = [
+        'user_id',
+        'user_ip'
+     ];
+
+}
+
